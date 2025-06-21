f3ab11291ec1a6a2425f72288dbc6eb2eb8dc711
lora-washington <168783011+lora-washington@users.noreply.github.com>
2025-06-21 19:41:17 +0200

    Update exchange.py

--- m1/utils/exchange.py
+++ m1/utils/exchange.py
@@ -1,11 +1,10 @@
-from websocket.bybit_ws_client import BybitWebSocketClient
+from websocket.bybit_ws_client import BybitWebSocketClient  # правильно импортируем
 
-def init_exchange(api_key, api_secret, symbol="USDT", is_testnet=False):
-    print(f"✅ Биржа инициализирована для пары: {symbol}")
+def init_exchange(api_key, api_secret, symbol="USDT", is_testnet=False, market_type="spot"):
     return BybitWebSocketClient(
         api_key=api_key,
         api_secret=api_secret,
         symbol=symbol,
         is_testnet=is_testnet,
-        market_type="spot"
+        market_type=market_type
     )
